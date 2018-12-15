8522949c630f63522642ea9c864d92c33d2b01d9
volerdman <37471065+volerdman@users.noreply.github.com>
2018-12-15 21:32:12 +0400

    Third_Laba

---
+++ First_Laba/First_Laba/BusStation.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Laba
+{
+    /// <summary>
+    /// Параметризованны класс для хранения набора объектов от интерфейса ITransport
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class BusStation<T> where T : class, ITransport
+    {
+        /// <summary>
+        /// Массив объектов, которые храним
+        /// </summary>
+        private T[] _places;
+
+        /// <summary>
+        /// Ширина окна отрисовки
+        /// </summary>
+        private int PictureWidth { get; set; }
+
+        /// <summary>
+        /// Высота окна отрисовки
+        /// </summary>
+        private int PictureHeight { get; set; }
+
+        /// <summary>
+        /// Размер парковочного места (ширина)
+        /// </summary>
+        private int _placeSizeWidth = 210;
+
+        /// <summary>
+        /// Размер парковочного места (высота)
+        /// </summary>
+        private int _placeSizeHeight = 80;
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="sizes">Количество мест на парковке</param>
+        /// <param name="pictureWidth">Рамзер парковки - ширина</param>
+        /// <param name="pictureHeight">Рамзер парковки - высота</param>
+        public BusStation(int sizes, int pictureWidth, int pictureHeight)
+        {
+            _places = new T[sizes];
+            PictureWidth = pictureWidth;
+            PictureHeight = pictureHeight;
+            for (int i = 0; i < _places.Length; i++)
+            {
+                _places[i] = null;
+            }
+        }
+
+        /// <summary>
+        /// Перегрузка оператора сложения
+        /// Логика действия: на парковку добавляется автобус
+        /// </summary>
+        /// <param name="p">Парковка</param>
+        /// <param name="car">Добавляемый автобус</param>
+        /// <returns></returns>
+        public static int operator +(BusStation<T> b, T bus)
+        {
+            for (int i = 0; i < b._places.Length; i++)
+            {
+                if (b.CheckFreePlace(i))
+                {
+                    b._places[i] = bus;
+                    b._places[i].SetPosition(5 + i / 5 * b._placeSizeWidth + 5,
+                     i % 5 * b._placeSizeHeight + 15, b.PictureWidth,
+                    b.PictureHeight);
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Перегрузка оператора вычитания
+        /// Логика действия: с парковки забираем автобус
+        /// </summary>
+        /// <param name="p">Парковка</param>
+        /// <param name="index">Индекс места, с которого пытаемся извлечь объект</param>
+        /// <returns></returns>
+        public static T operator -(BusStation<T> b, int index)
+        {
+            if (index < 0 || index > b._places.Length)
+            {
+                return null;
+            }
+            if (!b.CheckFreePlace(index))
+            {
+                T bus = b._places[index];
+                b._places[index] = null;
+                return bus;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Метод проверки заполнености парковочного места (ячейки массива)
+        /// </summary>
+        /// <param name="index">Номер парковочного места (порядковый номер в массиве)</param>
+        /// <returns></returns>
+        private bool CheckFreePlace(int index)
+        {
+            return _places[index] == null;
+        }
+
+        /// <summary>
+        /// Метод отрисовки парковки
+        /// </summary>
+        /// <param name="g"></param>
+        public void Draw(Graphics g)
+        {
+            DrawMarking(g);
+            for (int i = 0; i < _places.Length; i++)
+            {
+                if (!CheckFreePlace(i))
+                {//если место не пустое
+                    _places[i].DrawBus(g);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Метод отрисовки разметки парковочных мест
+        /// </summary>
+        /// <param name="g"></param>
+        private void DrawMarking(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black, 3);
+            //границы праковки
+            g.DrawRectangle(pen, 0, 0, (_places.Length / 5) * _placeSizeWidth, 480);
+            for (int i = 0; i < _places.Length / 5; i++)
+            {//отрисовываем, по 5 мест на линии
+                for (int j = 0; j < 6; ++j)
+                {//линия рамзетки места
+                    g.DrawLine(pen, i * _placeSizeWidth, j * _placeSizeHeight,
+                    i * _placeSizeWidth + 110, j * _placeSizeHeight);
+                }
+                g.DrawLine(pen, i * _placeSizeWidth, 0, i * _placeSizeWidth, 400);
+            }
+        }
+    }
+}
---
+++ First_Laba/First_Laba/FormBusStation.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace First_Laba
+{
+    partial class FormBusStation
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBoxStation = new System.Windows.Forms.PictureBox();
+            this.buttonSetBus = new System.Windows.Forms.Button();
+            this.buttonSetDoubleBus = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.maskedTextBox = new System.Windows.Forms.MaskedTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonTakeBus = new System.Windows.Forms.Button();
+            this.pictureBoxTakeBus = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStation)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeBus)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBoxStation
+            // 
+            this.pictureBoxStation.BackColor = System.Drawing.Color.Gray;
+            this.pictureBoxStation.Location = new System.Drawing.Point(1, 2);
+            this.pictureBoxStation.Name = "pictureBoxStation";
+            this.pictureBoxStation.Size = new System.Drawing.Size(720, 530);
+            this.pictureBoxStation.TabIndex = 0;
+            this.pictureBoxStation.TabStop = false;
+            // 
+            // buttonSetBus
+            // 
+            this.buttonSetBus.Location = new System.Drawing.Point(752, 22);
+            this.buttonSetBus.Name = "buttonSetBus";
+            this.buttonSetBus.Size = new System.Drawing.Size(110, 46);
+            this.buttonSetBus.TabIndex = 1;
+            this.buttonSetBus.Text = "Припарковать автобус";
+            this.buttonSetBus.UseVisualStyleBackColor = true;
+            this.buttonSetBus.Click += new System.EventHandler(this.buttonSetBus_Click);
+            // 
+            // buttonSetDoubleBus
+            // 
+            this.buttonSetDoubleBus.Location = new System.Drawing.Point(752, 86);
+            this.buttonSetDoubleBus.Name = "buttonSetDoubleBus";
+            this.buttonSetDoubleBus.Size = new System.Drawing.Size(110, 51);
+            this.buttonSetDoubleBus.TabIndex = 2;
+            this.buttonSetDoubleBus.Text = "Припарковать двухэтажный автобус";
+            this.buttonSetDoubleBus.UseVisualStyleBackColor = true;
+            this.buttonSetDoubleBus.Click += new System.EventHandler(this.buttonSetDoubleBus_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBoxTakeBus);
+            this.groupBox1.Controls.Add(this.buttonTakeBus);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.maskedTextBox);
+            this.groupBox1.Location = new System.Drawing.Point(727, 276);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(168, 245);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Забрать автобус:";
+            // 
+            // maskedTextBox
+            // 
+            this.maskedTextBox.Location = new System.Drawing.Point(54, 29);
+            this.maskedTextBox.Name = "maskedTextBox";
+            this.maskedTextBox.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Место:";
+            // 
+            // buttonTakeBus
+            // 
+            this.buttonTakeBus.Location = new System.Drawing.Point(94, 55);
+            this.buttonTakeBus.Name = "buttonTakeBus";
+            this.buttonTakeBus.Size = new System.Drawing.Size(62, 22);
+            this.buttonTakeBus.TabIndex = 2;
+            this.buttonTakeBus.Text = "Забрать";
+            this.buttonTakeBus.UseVisualStyleBackColor = true;
+            this.buttonTakeBus.Click += new System.EventHandler(this.buttonTakeBus_Click);
+            // 
+            // pictureBoxTakeBus
+            // 
+            this.pictureBoxTakeBus.BackColor = System.Drawing.Color.Gray;
+            this.pictureBoxTakeBus.Location = new System.Drawing.Point(9, 83);
+            this.pictureBoxTakeBus.Name = "pictureBoxTakeBus";
+            this.pictureBoxTakeBus.Size = new System.Drawing.Size(153, 156);
+            this.pictureBoxTakeBus.TabIndex = 3;
+            this.pictureBoxTakeBus.TabStop = false;
+            // 
+            // FormBusStation
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(895, 533);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.buttonSetDoubleBus);
+            this.Controls.Add(this.buttonSetBus);
+            this.Controls.Add(this.pictureBoxStation);
+            this.Name = "FormBusStation";
+            this.Text = "FormBusStation";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxStation)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxTakeBus)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBoxStation;
+        private System.Windows.Forms.Button buttonSetBus;
+        private System.Windows.Forms.Button buttonSetDoubleBus;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox pictureBoxTakeBus;
+        private System.Windows.Forms.Button buttonTakeBus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox;
+    }
+}
