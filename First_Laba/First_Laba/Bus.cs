56fbd4ac8b796c810b6cd396e7a21e5ad0736890
volerdman <37471065+volerdman@users.noreply.github.com>
2018-12-15 19:42:00 +0400

    Second_Laba

--- First_Laba/First_Laba/Bus.cs
+++ First_Laba/First_Laba/Bus.cs
@@ -7,73 +7,17 @@ using System.Threading.Tasks;
 
 namespace First_Laba
 {
-    public enum Direction
-    {
-        Up,
-        Diagonal,
-        Down,
-        Left,
-        Right
-    }
-
-    public class Bus
+    public class Bus:PublicTransport
     {
         /// <summary>
-        /// Левая координата отрисовки автобуса
+        /// Ширина отрисовки автобуса
         /// </summary>
-        private float _startPosX;
-
-        /// <summary>
-        /// Правая координата отрисовки автобуса
-        /// </summary>
-        private float _startPosY;
-
-        /// <summary>
-        /// Ширина окна отрисовки
-        /// </summary>
-        private int _pictureWidth;
-
-        //Высота окна отрисовки
-        private int _pictureHeight;
+        protected const int busWidth = 85;
 
         /// <summary>
         /// Ширина отрисовки автобуса
         /// </summary>
-        private const int busWidth = 85;
-
-        /// <summary>
-        /// Ширина отрисовки автобуса
-        /// </summary>
-        private const int busHeight = 50;
-
-        /// <summary>
-        /// Максимальная скорость
-        /// </summary>
-        public int MaxSpeed { private set; get; }
-
-        /// <summary>
-        /// Вес автобуса
-        /// </summary>
-        public float Weight { private set; get; }
-
-        /// <summary>
-        /// Основной цвет кузова
-        /// </summary>
-        public Color MainColor { private set; get; }
-        /// <summary>
-        /// Дополнительный цвет
-        /// </summary>
-        public Color DopColor { private set; get; }
-
-        /// <summary>
-        /// Признак наличия второго этажа
-        /// </summary>
-        public bool SecondFloor { private set; get; }
-
-        /// <summary>
-        /// Признак наличия тонировки
-        /// </summary>
-        public bool BlackWindow { private set; get; }
+        protected const int busHeight = 50;
 
         /// <summary>
         /// Конструктор
@@ -84,37 +28,18 @@ namespace First_Laba
         /// <param name="dopColor">Дополнительный цвет</param>
         /// <param name="SecondFloor">Признак наличия второго этажа</param>
         /// <param name="BlackWindow">Признак наличия тонировки</param>
-        public Bus(int maxSpeed, float weight, Color mainColor, Color dopColor, bool secondFloor, bool blackWindow)
+        public Bus(int maxSpeed, float weight, Color mainColor)
         {
             MaxSpeed = maxSpeed;
             Weight = weight;
             MainColor = mainColor;
-            DopColor = dopColor;
-            SecondFloor = secondFloor;
-            BlackWindow = blackWindow;
         }
-
-        /// <summary>
-        /// Установка позиции автобуса
-        /// </summary>
-        /// <param name="x">Координата X</param>
-        /// <param name="y">Координата Y</param>
-        /// <param name="width">Ширина картинки</param>
-        /// <param name="height">Высота картинки</param>
-        public void SetPosition(int x, int y, int width, int height)
-        {
-            _startPosX = x;
-            _startPosY = y;
-            _pictureWidth = width;
-            _pictureHeight = height;
-        }
-
 
         /// <summary>
         /// Изменение направления пермещения
         /// </summary>
         /// <param name="direction">Направление</param>
-        public void MoveTransport(Direction direction)
+        public override void MoveTransport(Direction direction)
         {
             float step = MaxSpeed * 100 / Weight;
             switch (direction)
@@ -133,17 +58,9 @@ namespace First_Laba
                         _startPosX -= step;
                     }
                     break;
-                case Direction.Diagonal:
-                    if ((_startPosY - step + 5 > 0) && (_startPosX + step < _pictureWidth - busWidth + 5))
-                    {
-                        _startPosY -= step;
-                        _startPosX += step;
-                    }
-                    break;
-
                 //вверх
                 case Direction.Up:
-                    if (_startPosY - step + 5 > 0)
+                    if (_startPosY - step + 25 > 0)
                     {
                         _startPosY -= step;
                     }
@@ -159,33 +76,17 @@ namespace First_Laba
             }
         }
 
-
-
         /// <summary>
         /// Отрисовка автобуса
         /// </summary>
         /// <param name="g"></param>
-        public void DrawBus(Graphics g)
+        public override void DrawBus(Graphics g)
         {
             Pen pen = new Pen(Color.Black);
             Brush br = new SolidBrush(MainColor);
-            Brush dop = new SolidBrush(DopColor);
             Brush back = new SolidBrush(Color.Silver);
             Brush black = new SolidBrush(Color.Black);
             Brush blue = new SolidBrush(Color.Blue);
-
-            // отрисуем сперва второй этаж (чтобы потом отрисовка первого на него "легла")
-            if (SecondFloor)
-            {
-                g.FillEllipse(br, _startPosX, _startPosY, 20, 40);
-                g.FillEllipse(br, _startPosX + 65, _startPosY, 20, 40);
-                g.FillRectangle(br, _startPosX + 10, _startPosY, 65, 25);
-                g.DrawRectangle(pen, _startPosX + 5, _startPosY + 10, 10, 10);
-                g.DrawRectangle(pen, _startPosX + 25, _startPosY + 10, 10, 10);
-                g.DrawRectangle(pen, _startPosX + 45, _startPosY + 10, 10, 10);
-                g.DrawRectangle(pen, _startPosX + 65, _startPosY + 10, 10, 10);
-
-            }
 
             //теперь отрисуем первый этаж автобуса
             g.FillRectangle(br, _startPosX + 1, _startPosY + 25, 84, 20);
@@ -193,37 +94,18 @@ namespace First_Laba
             g.DrawRectangle(pen, _startPosX + 25, _startPosY + 27, 10, 10);
             g.DrawRectangle(pen, _startPosX + 45, _startPosY + 27, 10, 10);
             g.DrawRectangle(pen, _startPosX + 68, _startPosY + 25, 12, 12);
-            g.FillRectangle(dop, _startPosX + 1, _startPosY + 22, 84, 2);
+            g.FillRectangle(black, _startPosX + 1, _startPosY + 22, 84, 2);
             g.FillEllipse(black, _startPosX + 11, _startPosY + 38, 12, 12);
             g.FillEllipse(black, _startPosX + 71, _startPosY + 38, 12, 12);
             g.FillEllipse(back, _startPosX + 13, _startPosY + 40, 8, 8);
             g.FillEllipse(back, _startPosX + 73, _startPosY + 40, 8, 8);
             g.FillRectangle(back, _startPosX + 82, _startPosY + 25, 86, 10);
 
-            //тонировка
-
-            if (BlackWindow)
-            {
-                g.FillRectangle(black, _startPosX + 5, _startPosY + 10, 10, 10);
-                g.FillRectangle(black, _startPosX + 25, _startPosY + 10, 10, 10);
-                g.FillRectangle(black, _startPosX + 45, _startPosY + 10, 10, 10);
-                g.FillRectangle(black, _startPosX + 65, _startPosY + 10, 10, 10);
-                g.FillRectangle(black, _startPosX + 5, _startPosY + 27, 10, 10);
-                g.FillRectangle(black, _startPosX + 25, _startPosY + 27, 10, 10);
-                g.FillRectangle(black, _startPosX + 45, _startPosY + 27, 10, 10);
-                g.FillRectangle(black, _startPosX + 68, _startPosY + 25, 12, 12);
-            }
-            else
-            {
-                g.FillRectangle(blue, _startPosX + 5, _startPosY + 10, 10, 10);
-                g.FillRectangle(blue, _startPosX + 25, _startPosY + 10, 10, 10);
-                g.FillRectangle(blue, _startPosX + 45, _startPosY + 10, 10, 10);
-                g.FillRectangle(blue, _startPosX + 65, _startPosY + 10, 10, 10);
-                g.FillRectangle(blue, _startPosX + 5, _startPosY + 27, 10, 10);
-                g.FillRectangle(blue, _startPosX + 25, _startPosY + 27, 10, 10);
-                g.FillRectangle(blue, _startPosX + 45, _startPosY + 27, 10, 10);
-                g.FillRectangle(blue, _startPosX + 68, _startPosY + 25, 12, 12);
-            }
+            //окна
+            g.FillRectangle(black, _startPosX + 5, _startPosY + 27, 10, 10);
+            g.FillRectangle(black, _startPosX + 25, _startPosY + 27, 10, 10);   
+            g.FillRectangle(black, _startPosX + 45, _startPosY + 27, 10, 10);
+            g.FillRectangle(black, _startPosX + 68, _startPosY + 25, 12, 12);
         }
     }
 }
---
+++ First_Laba/First_Laba/DoubleBus.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace First_Laba
+{
+    public enum Direction
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public class DoubleBus : Bus
+    {
+        /// <summary>
+        /// Дополнительный цвет
+        /// </summary>
+        public Color DopColor { private set; get; }
+
+        /// <summary>
+        /// Признак наличия второго этажа
+        /// </summary>
+        public bool SecondFloor { private set; get; }
+
+        /// <summary>
+        /// Признак наличия тонировки
+        /// </summary>
+        public bool BlackWindow { private set; get; }
+
+        /// <summary>
+        /// Конструктор
+        /// </summary>
+        /// <param name="dopColor">Дополнительный цвет</param>
+        /// <param name="SecondFloor">Признак наличия второго этажа</param>
+        /// <param name="BlackWindow">Признак наличия тонировки</param>
+        public DoubleBus(int maxSpeed, float weight, Color mainColor, Color dopColor, bool
+            secondFloor, bool blackWindow) :
+            base(maxSpeed, weight, mainColor)
+        {
+            DopColor = dopColor;
+            SecondFloor = secondFloor;
+            BlackWindow = blackWindow;
+        }
+
+        public override void MoveTransport(Direction direction)
+        {
+            float step = MaxSpeed * 100 / Weight;
+            switch (direction)
+            {
+                // вправо
+                case Direction.Right:
+                    if (_startPosX + step < _pictureWidth - busWidth + 5)
+                    {
+                        _startPosX += step;
+                    }
+                    break;
+                //влево
+                case Direction.Left:
+                    if (_startPosX - step + 5 > 0)
+                    {
+                        _startPosX -= step;
+                    }
+                    break;
+                //вверх
+                case Direction.Down:
+                    if (_startPosY + step - 7.5 < _pictureHeight - busHeight)
+                    {
+                        _startPosY += step;
+                    }
+                    break;
+                //вниз
+                case Direction.Up:
+                    if (_startPosY - step + 5 > 0)
+                    {
+                        _startPosY -= step;
+                    }
+                    break;
+                   
+            }
+        }
+
+        /// <summary>
+        /// Отрисовка автобуса
+        /// </summary>
+        /// <param name="g"></param>
+        public override void DrawBus(Graphics g)
+        {
+            Pen pen = new Pen(Color.Black);
+            Brush dop = new SolidBrush(DopColor);
+            Brush black = new SolidBrush(Color.Black);
+            Brush blue = new SolidBrush(Color.Blue);
+
+            // отрисуем сперва второй этаж (чтобы потом отрисовка первого на него "легла")
+            if (SecondFloor)
+            {
+                g.FillEllipse(dop, _startPosX, _startPosY, 20, 40);
+                g.FillEllipse(dop, _startPosX + 65, _startPosY, 20, 40);
+                g.FillRectangle(dop, _startPosX + 10, _startPosY, 65, 25);
+                g.DrawRectangle(pen, _startPosX + 5, _startPosY + 10, 10, 10);
+                g.DrawRectangle(pen, _startPosX + 25, _startPosY + 10, 10, 10);
+                g.DrawRectangle(pen, _startPosX + 45, _startPosY + 10, 10, 10);
+                g.DrawRectangle(pen, _startPosX + 65, _startPosY + 10, 10, 10);
+
+            }
+
+            //отрисуем основной кузов
+            base.DrawBus(g);
+
+            //отрисуем окна
+            g.FillRectangle(blue, _startPosX + 5, _startPosY + 10, 10, 10);
+            g.FillRectangle(blue, _startPosX + 25, _startPosY + 10, 10, 10);
+            g.FillRectangle(blue, _startPosX + 45, _startPosY + 10, 10, 10);
+            g.FillRectangle(blue, _startPosX + 65, _startPosY + 10, 10, 10);
+
+            //тонировка
+            if (BlackWindow)
+            {
+                g.FillRectangle(black, _startPosX + 5, _startPosY + 10, 10, 10);
+                g.FillRectangle(black, _startPosX + 25, _startPosY + 10, 10, 10);
+                g.FillRectangle(black, _startPosX + 45, _startPosY + 10, 10, 10);
+                g.FillRectangle(black, _startPosX + 65, _startPosY + 10, 10, 10);
+                g.FillRectangle(black, _startPosX + 5, _startPosY + 27, 10, 10);
+                g.FillRectangle(black, _startPosX + 25, _startPosY + 27, 10, 10);
+                g.FillRectangle(black, _startPosX + 45, _startPosY + 27, 10, 10);
+                g.FillRectangle(black, _startPosX + 68, _startPosY + 25, 12, 12);
+            }
+        }
+    }
+}
